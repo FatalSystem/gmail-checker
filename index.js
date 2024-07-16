f8f93d163f415ffdbd2ade657a42d7f0d92e7178
FatalSystem <168446442+FatalSystem@users.noreply.github.com>
2024-07-16 20:22:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-var http = require('http');
-
-//create a server object:
-http.createServer(function (req, res) {
-  res.write('** Welcome to IBD!!!! **'); //write a response to the client
-  res.end(); //end the response
-}).listen(80); //the server object listens on port 80
 
 const fs = require("fs");
 const readline = require("readline");
